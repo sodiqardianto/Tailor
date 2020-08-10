8c272792015f6cea678db651ded7d305ada72584
Sodiq Ardianto <sodiqardianto@gmail.com>
2020-08-11 00:34:28 +0700

    Menambahkan template adminlte

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Dashboard'
+        ];
+
+        return view('dashboard', $data);
+    }
+
+    //--------------------------------------------------------------------
+
+}
---
+++ app/Views/home/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<h1>Kosong</h1>
+
+<?= $this->endSection(); ?>
