12135eaf17e2c6a9c26d0ded189e9ee52ccd9426
Sodiq Ardianto <sodiqardianto@gmail.com>
2020-08-13 01:29:54 +0700

    Menambahkan daftar-daftar dan barang

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -13,6 +13,42 @@ class Dashboard extends BaseController
         return view('dashboard', $data);
     }
 
+    public function barangMasuk()
+    {
+        $data = [
+            'title' => 'Barang Masuk'
+        ];
+
+        return view('barang/barang_masuk', $data);
+    }
+
+    public function daftarHarga()
+    {
+        $data = [
+            'title' => 'Daftar Harga'
+        ];
+
+        return view('daftar/daftar_harga', $data);
+    }
+
+    public function daftarPekerja()
+    {
+        $data = [
+            'title' => 'Daftar Pekerja'
+        ];
+
+        return view('daftar/daftar_pekerja', $data);
+    }
+
+    public function daftarSeller()
+    {
+        $data = [
+            'title' => 'Daftar Seller'
+        ];
+
+        return view('daftar/daftar_seller', $data);
+    }
+
     //--------------------------------------------------------------------
 
 }
